1747d6c730e09ee1146027aab6ad6ee157d14967
Ludo <zaina4691@gmail.com>
2021-11-27 16:53:59 +0500

    Initial Commit

---
+++ src/fee/Patient.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fee;
+class Patient {
+    public String name;
+    public String gender;
+    public String PhoneNo;
+    public String Email;
+    public int FileNo;
+    public String DateOFBirth;
+    public int Insurance;
+    public int IDNo;
+    public int Nationality;
+    
+}
